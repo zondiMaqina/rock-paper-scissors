b5722c8b5e15cc46fddfee6de6e618234cd89475
zondimaqina <zondimaqina0@gmail.com>
2024-03-11 11:19:26 +0200

    add function for computor random choices

---
+++ main.js
@@ -0,0 +1,22 @@
+//Program must run on console
+// return ll values first
+
+// comoutor must generate random choices
+//  "rock", "papers" or "scissors"
+// add function that plays a single round
+// function must have parameters for:
+//  playerselection choice
+//   must be case-insensitive
+//  computor slecetion choice
+//  function must respond with:
+//  "You win" or "You lose" or "Its a Tie"
+
+
+function getComputerChoice(){
+    let computor_choices = ["rock", "paper", "scissors"] ;
+
+    let random_choice = computor_choices[Math.floor(Math.random() * computor_choices.length)]
+
+    return random_choice
+}
+
